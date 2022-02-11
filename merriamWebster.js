873abd21d2c5bfe82a9c067209f2fbf7312b6e3f
Fredrick Esposito <fredrickesposito@gmail.com>
2022-02-11 01:02:12 -0500

    Scrape some words from Merriam-Webster
    Just the a and b words for now to make testing more efficient. First the following npm packages were installed for scraping: cheerio, request, request-promise. Then the browserify npm package was installed to generate merriamWebsterBundle.js from merrianWebster.js to allow the corresponding NodeJS code to be bundled for execution in the browser.

--- merriamWebster.js
+++ merriamWebster.js
@@ -1,3 +1,66 @@
-function getWordsFromMerriamWebster() {
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+class WordScrape {
+    constructor(letter) {
+        this.letter = letter;
+        this.pageNum = 1;
+        this.website = 'https://www.merriam-webster.com';
+        this.page = '/browse/dictionary/';
+    }
+
+    scrape(onComplete) {
+        var words = [];
+        var f = (error, response, html) => {
+            if(!error && response.statusCode == 200) {
+                const $= cheerio.load(html);
+                $(".entries").each((i, data) => {
+                    const item = $(data).text().replace(/^\s+|\s+$/gm,'');
+                    if(!this.hasNumber(item)) {
+                        words.push(item);
+                    }
+                })
+                this.pageNum++;
+                var nextPageLink = this.page + this.letter + "/" + this.pageNum;
+                var foundNextPageLink = false;
+                $("a").each((i, data) => {
+                    var link = ($(data).attr('href'));
+                    if(link == nextPageLink) {
+                        request(this.website + link, f);
+                        foundNextPageLink = true;
+                        return false;
+                    }
+                });
+                if(!foundNextPageLink) {
+                    onComplete(words);
+                }
+            }
+        };
+        var initialLink = this.website + this.page + this.letter;
+        request(initialLink, f);
+    }
+
+    hasNumber(myString) {
+        return /\d/.test(myString);
+    }
+}
+
+window.getWordsFromMerriamWebster = function() {
+    var letters = ['a', 'b', /*'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'*/];
+    var letterIdx = 0;
+    var allWords = [];
+    var onComplete = function(words) {
+        console.log("Got words for " + letters[letterIdx] + ": " + "Found " + words.length + " words!");
+        allWords.push(words);
+        if(++letterIdx < letters.length) {
+            new WordScrape(letters[letterIdx]).scrape(onComplete);
+        }
+        else {
+            console.log("DONE");
+            allWords = allWords.flat();
+            console.log("Found a total of " + allWords.length + " words!");
+        }
+    };
+    new WordScrape(letters[0]).scrape(onComplete);
     return ['test'];
 }
