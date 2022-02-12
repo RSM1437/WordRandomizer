9d7a216c634ff8d757d1f54f5090ef25fbc5cdbf
Fredrick Esposito <fredrickesposito@gmail.com>
2022-02-11 23:57:47 -0500

    Update merriamWebsterBundle.js

--- merriamWebsterBundle.js
+++ merriamWebsterBundle.js
@@ -17,7 +17,7 @@ class WordScrape {
     }
 
     shouldAdd(word) {
-        if(this.hasNumber(word)) {
+        if(this.hasNumber(word) || this.hasUnsupportedChars(word)) {
             return false;
         }
 
@@ -57,11 +57,9 @@ class WordScrape {
                     const item = $(data).text().replace(/^\s+|\s+$/gm,'');
                     var newWords = item.split('\n');
                     newWords.forEach(newWord => {
+                        newWord = this.cleanSpecialChars(newWord);
                         if(this.shouldAdd(newWord)) {
                             words.push(newWord);
-                        }
-                        else {
-                            console.log("ignored " + newWord);
                         }
                     });
                 })
@@ -89,6 +87,17 @@ class WordScrape {
         return /\d/.test(myString);
     }
 
+    //jspdf doesn't support UTF-8 characters :(
+    cleanSpecialChars(string) {
+        var newString = string.replace('ʽ', '\'');
+        return newString;
+    }
+
+    //jspdf doesn't support UTF-8 characters :(
+    hasUnsupportedChars(string) {
+        return/[^\x00-\x7F]/g.test(string);
+    }
+
     isLetter(c) {
         return c.toLowerCase() != c.toUpperCase();
     }
@@ -105,11 +114,23 @@ class WordScrape {
         return word.includes("-") && !word.startsWith("-") && !word.endsWith("-");
     }
 
+    indexOfFirstLetter(string) {
+        var firstLetterIndex = null;
+        for(let i = 0; i < string.length; i++) {
+            if(this.isLetter(string.charAt(i))) {
+                firstLetterIndex = i;
+                break;
+            }
+        }
+        return firstLetterIndex;
+    }
+
     isProper(word) {
-        if(!this.isUpperCaseLetter(word.charAt(0))) {
+        var firstLetterAt = this.indexOfFirstLetter(word);
+        if(firstLetterAt == null || !this.isUpperCaseLetter(word.charAt(firstLetterAt))) {
             return false;
         }
-        for(let i = 1; i < word.length; i++) {
+        for(let i = firstLetterAt + 1; i < word.length; i++) {
             if(this.isLetter(word.charAt(i)) && !this.isLowerCaseLetter(word.charAt(i))) {
                 return false;
             }
@@ -130,17 +151,30 @@ class WordScrape {
     }
 
     isAcronym(word) {
-        for (let i = 0; i < word.length; i++) {
-            if(!this.isUpperCaseLetter(word.charAt(i)) && word.charAt(i) != ".") {
-                return false;
+        var isLowercaseAcronym = true;
+        for(let i = 0; i < word.length; i += 2) {
+            if(i+1 >= word.length || !this.isLetter(word.charAt(i)) || word.charAt(i+1) != '.') {
+                isLowercaseAcronym = false;
+                break;
             }
+        }
+
+        var isUpeprcaseAcronym = true;
+        for (let i = 0; i < word.length; i++) {
+            if(!this.isUpperCaseLetter(word.charAt(i)) && word.charAt(i) != "." && word.charAt(i) != '/' && word.charAt(i) != '&' && (i != word.length - 1 || word.charAt(i) != 's') && (i != word.length - 2 || word.charAt(i) != '\'')) {
+                isUpeprcaseAcronym = false;
+                break;
+            }
+        }
+        if(!isLowercaseAcronym && !isUpeprcaseAcronym) {
+            return false;
         }
         return true;
     }
 }
 
 window.getWordsFromMerriamWebster = function(includeHyphenated, includeProper, includePhrases, includePrefixes, includeSuffixes, includeAcronyms, onProgress, onComplete) {
-    var letters = ['a', /*'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'*/];
+    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
     var letterIdx = 0;
     var allWords = [];
     var letterIsDoneCallback = function(words) {
